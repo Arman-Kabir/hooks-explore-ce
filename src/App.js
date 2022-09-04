dee3658ef916c882df930c35046b60140fb93020
Arman-Kabir <armankabir8@gmail.com>
2022-09-04 11:13:04 +0600

    exploring useState with array

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import ClassCounter from './components/useState/ClassCounter';
 import HookCounter from './components/useState/HookCounter';
+import HookCounterFour from './components/useState/HookCounterFour';
 import HookCounterThree from './components/useState/HookCounterThree';
 import HookCounterTwo from './components/useState/HookCounterTwo';
 
@@ -10,7 +11,8 @@ function App() {
       {/* <ClassCounter></ClassCounter> */}
       {/* <HookCounter></HookCounter> */}
       {/* <HookCounterTwo></HookCounterTwo> */}
-      <HookCounterThree></HookCounterThree>
+      {/* <HookCounterThree></HookCounterThree> */}
+      <HookCounterFour></HookCounterFour>
     </div>
   );
 }
