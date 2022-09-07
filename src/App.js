d663cb4daf7f58dcae865a16bb3ba6769665f431
Arman-Kabir <armankabir8@gmail.com>
2022-09-07 10:27:31 +0600

    few useeffect explored

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import './App.css';
+import ClassCounterOne from './components/useEffect/ClassCounterOne';
+import HookCounterOne from './components/useEffect/HookCounterOne';
 import ClassCounter from './components/useState/ClassCounter';
 import HookCounter from './components/useState/HookCounter';
 import HookCounterFour from './components/useState/HookCounterFour';
@@ -13,6 +15,9 @@ function App() {
       {/* <HookCounterTwo></HookCounterTwo> */}
       {/* <HookCounterThree></HookCounterThree> */}
       {/* <HookCounterFour></HookCounterFour> */}
+
+      {/* <ClassCounterOne></ClassCounterOne> */}
+      <HookCounterOne></HookCounterOne>
     </div>
   );
 }
